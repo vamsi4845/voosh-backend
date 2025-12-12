9928d1498ac9fe4269f99fafc8e0ae8726dde3fb
vamsisai <vamsisai.karumuri@gmail.com>
2025-12-12 21:58:49 +0530

    chore: init express project

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
